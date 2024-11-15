7c1268cd6bc9198d1fb14b07d306c98b740afb13
joaquinlima777 <joaquinlima172008@gmail.com>
2024-11-14 22:45:21 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+console.log('JavaScript activo');
+
+function resetNavPosition() {
+    const nav = document.querySelector('nav4');
+    nav.style.position = 'relative';
+    nav.style.bottom = '';
+    nav.style.left = '';
+}
